0d79acc689ce4ba44366cdab7543eac7f08c0d80
hanachi-ap <hanachi.ap@gmail.com>
2023-10-09 22:49:11 +0900

    [Ergotonic49] Update to support qmk 0.22

--- qmk_firmware/keyboards/hanachi_ap/ergotonic49/rules.mk
+++ qmk_firmware/keyboards/hanachi_ap/ergotonic49/rules.mk
@@ -1,32 +1,12 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = atmel-dfu
-
-# Build Options
-#   change yes to no to disable
-#
-BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
-AUDIO_ENABLE = no           # Audio output
-
+# This file intentionally left blank
 ENCODER_ENABLE = yes
-SRC += matrix.c 
-CUSTOM_MATRIX = yes
+ENCODER_MAP_ENABLE = yes
 
-VIA_ENABLE = yes
+VIA_ENABLE = no
 AUTO_SHIFT_ENABLE = no
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 
-# Reduce firmware size: enable Link-Time-Optimization
-LTO_ENABLE = yes
+
+SRC += matrix.c matrix_common.c
+CUSTOM_MATRIX = yes
